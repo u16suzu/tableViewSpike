428bcc745d86d7b6375f3bdf7a85720e5274d42e
u16suzu <u16suzu@gmail.com>
2013-09-15 02:37:35 +0900

    1st

---
+++ tableViewSpike/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  tableViewSpike
+//
+//  Created by u16suzu on 2013/09/15.
+//  Copyright (c) 2013年 u16suzu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ tableViewSpike/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  tableViewSpike
+//
+//  Created by u16suzu on 2013/09/15.
+//  Copyright (c) 2013年 u16suzu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
