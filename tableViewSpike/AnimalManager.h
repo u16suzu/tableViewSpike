744b5f134a709fcf3d0a62468cd34a9eabb8fea0
u16suzu <u16suzu@gmail.com>
2013-09-16 23:17:33 +0900

    Add: dispatch_once spike
    cosmetic

---
+++ tableViewSpike/AnimalManager.h
@@ -0,0 +1,16 @@
+//
+//  Animal.h
+//  tableViewSpike
+//
+//  Created by u16suzu on 2013/09/16.
+//  Copyright (c) 2013年 u16suzu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AnimalManager : NSObject
+
++ (AnimalManager *)sharedManager;
+
+@end
+
